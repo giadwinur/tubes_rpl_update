e0a9f14dc0121b51dd0220c57bb14bf66a52a7bf
Dhea Nurlita <dheanurlitasa23@gmail.com>
2023-11-24 18:08:55 +0700

    percobaan

---
+++ lib/myapp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tubes_rpl_update/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: const Home());
+  }
+}
